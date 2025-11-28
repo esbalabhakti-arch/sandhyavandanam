8d3747f558b85a5d721df5cf2fb61118d3da85ae
esbalabhakti-arch <esbalabhakti@gmail.com>
2025-11-27 18:55:40 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+// data.js
+// BPHS Flash Cards – Chapter 1 fully populated
+// You can later append more chapters to the `chapters` array.
+
+const chapters = [
+  //New Chapter begins
+   {
+id: "ch1",
+    title: "Podcast 1 – Intro, Achamanam, Ganapati Dhyanam",
+    description:
+      "Kid-friendly podcast script explaining Ācamana, the 12 Viṣṇu names with body-touch meanings, Gaṇapati Dhyānam with head-tapping with full word-by-word meanings.",
+    audio: [
+          { label: "Podcast 1 – Intro, Achamanam, Ganapati Dhyanam", file: "audio/101_SV_upto_Ganapatidhyanam.opus" },
+    ],
+    cards: [
+      {
+        id: "ch1",
+        //type: "verse",
+        //verseNumber: 46,
+        sanskrit:
+		"<img src='images/101_Podcast1_Fr.jpg' />",
+        //hint: "Opening scene – who bows and to whom?",
+        translation:
+		"<img src='images/101_Podcast1_Bk.jpg' />",
+        wordByWord:
+" "
+      }
+]
+}
+//Chapter Ends
+];
